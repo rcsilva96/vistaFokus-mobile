7dea95b2d92a9ccc706e61baa19c449360860c21
Renato Carvalho <rsilv1822@gmail.com>
2025-10-01 18:34:02 -0300

    useState configured to timer, testing in seconds yet

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,56 @@
+import { useState } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
+const pomodoro = [
+  {
+    id: 'focus',
+    initialValue: 25,
+    image: require('./assets/img/luciaPomodoro.png'),
+  },
+  {
+    id: 'shortBreak',
+    initialValue: 5,
+    image: require('./assets/img/LuciaPausaCurta.png'),
+  },
+  {
+    id: 'longBreak',
+    initialValue: 15,
+    image: require('./assets/img/LuciaPausaLonga.png'),
+  }
+]
+
 export default function Index() {
+
+  const [timerType, setTimerType] = useState(pomodoro[0]);
+
   return (
     <View
       style={styles.container}
     >
-      <Image 
-        source = {require('./assets/img/luciaPomodoro.png')}
-        style = {{ width : 300,  height : 300 }}
+      <Image
+        source={timerType.image}
+        style={{ width: 300, height: 300 }}
       />
 
       <View style={styles.actions}>
 
-        <Text style={styles.timer}>25:00</Text>
+        <View style={styles.context}>
+          <Pressable style={styles.contextButtonActive}>
+            <Text style={styles.contextButtonText}>Foco</Text>
+          </Pressable>
+
+          <Pressable>
+            <Text style={styles.contextButtonText}>Pausa curta</Text>
+          </Pressable>
+
+          <Pressable>
+            <Text style={styles.contextButtonText}>Pausa longa</Text>
+          </Pressable>
+        </View>
+
+        <Text style={styles.timer}>
+          { new Date(timerType.initialValue * 1000).toLocaleTimeString('pt-BR', { minute: '2-digit', second: '2-digit' }) }
+        </Text>
 
         <Pressable style={styles.btn}>
           <Text style={styles.btnText}>Iniciar</Text>
@@ -20,7 +58,7 @@ export default function Index() {
 
       </View>
 
-      <View style ={styles.footer}>
+      <View style={styles.footer}>
 
         <Text style={styles.footerText}>TechVista, 2025</Text>
 
@@ -53,11 +91,11 @@ const styles = StyleSheet.create({
     gap: 32
 
   },
-  
+
   text: {
 
     color: '#FFF'
-    
+
   },
 
   timer: {
@@ -98,6 +136,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#98A0A8',
     fontSize: 14
-  }
+  },
+
+  context:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItens: 'center'
+  },
+
+  contextButtonActive:{
+    backgroundColor: '#144480',
+    borderRadius: 8
+  },
+
+  contextButtonText:{
+    fontSize: 12.5,
+    color: '#FFF',
+    padding: 8,
+  },
 
 })
