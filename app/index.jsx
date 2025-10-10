d37b3ee9f6301563cf5f6b9df82cba4f8d988532
Renato Carvalho <rsilv1822@gmail.com>
2025-10-09 23:29:37 -0300

    initial screen and routing are working

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,81 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Text, View, Image, StyleSheet, Dimensions } from "react-native";
+import VistaFoKusButton from "./components/VistaFokusButton";
+import { useRouter } from "expo-router";
+
+const { width, height } = Dimensions.get('window');
 
 export default function Index() {
+  const router = useRouter();
   return (
+    <View style={styles.container}>
+      <Image 
+        source={require('../assets/images/logo.png')} 
+        style={styles.logo}
+        resizeMode="contain"
+      />
 
-    <View>
+      <Text style={styles.title}>
+        Otimize sua {'\n'} produtividade, {'\n'}focando no que {'\n'} importa.
+      </Text>
 
-      <Text>Página Inicial</Text>
-      <Link href={ { pathname: '/components/Pomodoro/pomodoro' } }> Focar </Link>
+      <View style={styles.luciaContainer}>
+        <Image 
+          source={require('../assets/images/lucia/luciaPomodoro.png')}
+          style={styles.lucia}
+          resizeMode="contain"
+        />
+        <View style={styles.buttonWrapper}>
+          <VistaFoKusButton 
+          
+            title="        Vamos lá!        "
+            onPress={() => router.push('/components/Pomodoro/pomodoro')} />
+        </View>
+      </View>
 
+      <Text style={styles.footerText}>TechVista, 2025</Text>
     </View>
+  );
+}
 
-  )
-  }
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: '#1A1237',
+    paddingVertical: 60,
+  },
+  logo: {
+    width: width * 0.7,
+    height: height * 0.1,
+    marginBottom: 10,
+  },
+  title: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    lineHeight: 28,
+    marginBottom: 30,
+  },
+  luciaContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  lucia: {
+    width: width * 0.65,
+    height: height * 0.35,
+    marginBottom: -35, // 👈 faz a imagem “encostar” no botão
+  },
+  buttonWrapper: {
+    zIndex: 1,
+  },
+  footerText: {
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 14,
+    position: 'absolute',
+    bottom: 20,
+  },
+});
